f3498d638437d85a8ed5cf332e9ce246d506bf76
Filippo Casadei <casadeifilippo91@gmail.com>
2026-04-01 17:30:22 +0200

    risolta task 5, era praticamente uguale alla 1, + if/else ma ho dovuto cercare il costrutto e il metodo per per creare data/ora ed estrarre le ore.

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -1,4 +1,5 @@
-/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito. 
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto 
+seguito dal nome fornito. 
 Il saluto deve essere :
 buongiorno se è mattina (fino alle 13), 
 buon pomeriggio se è pomeriggio (fino alle 17) 
@@ -9,10 +10,23 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
+function saluto(nome) {
+const ora = new Date().getHours();
+    if (ora <= 13) {
+        return ("Buongiorno " + nome)
+    }
+    else if (ora <= 17 ){
+        return ("Buon pomeriggio " + nome)
+    } 
+    else {
+        return ("Buonasera " + nome)
+    } 
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+saluto(name);
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
+console.log(saluto(name));
