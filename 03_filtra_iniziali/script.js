e42d624c172b3603c0d05aba833e4592cc0bcd70
Filippo Casadei <casadeifilippo91@gmail.com>
2026-04-01 16:42:40 +0200

    risolto task 3: - dichiarata la funzione - invocata la funzione - stampata in console

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,13 +1,24 @@
-/* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
+/* Scrivi una funzione che accetti un'array di stringhe e una lettera
+ e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function nuovoArray(array, lettera){
+    let arrayVuoto = [];
+    for (let i = 0; i < array.length ; i++) {
+        if ( array[i][0] === lettera){
+            arrayVuoto.push(array[i])
+        }
+    }
+        return arrayVuoto
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+nuovoArray(names,"A");
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
+
+console.log(nuovoArray(names,"A"));
