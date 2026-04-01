c4f2629026cdbb22f2056360317de99dc81e7968
Filippo Casadei <casadeifilippo91@gmail.com>
2026-04-01 17:08:58 +0200

    risolta task 4.

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,28 @@
-/* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
+/* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali 
+contenute al suo interno */
 
 const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function contaVocali(stringa) {
+    let contenitoreVocali = 0;
+    for (let i = 0; i < stringa.length; i++) {
+        if (stringa[i] === "a" ||
+            stringa[i] === "e" ||
+            stringa[i] === "i" ||
+            stringa[i] === "o" ||
+            stringa[i] === "u") {
+        contenitoreVocali++  
+        }
+    }
+    return contenitoreVocali
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+contaVocali("javascript");
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+console.log(contaVocali(word));
